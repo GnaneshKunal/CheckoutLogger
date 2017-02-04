62b2038109615e0a3fff922f4401354e6441c716
gnaneshkunal <gnaneshpingu@gmail.com>
2017-02-05 00:22:28 +0530

    testing route

--- index.js
+++ index.js
@@ -2,20 +2,39 @@ const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const config = require('./config');
 
-mongoose.connect('mongodb://' + config.user + ':' + config.password + '@localhost:27017' + '/' + config.database, (err) => {
-    if (err) throw err;
-    console.log('Connected to database');
-})
+const User = require('./models/user');
 
 const app = express();
 
+mongoose.connect('mongodb://' + config.user + ':' + config.password + '@localhost:27017' + '/' + config.database, (err) => {
+    if (err) throw err;
+    else console.log('Connected to database');
+});
+
 //Middlewares
 app.use(morgan('tiny'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post('/create', (req, res, next) => {
+    var user = new User();
+
+    user.profile.name = req.body.name;
+    user.email = req.body.email;
+    user.password = req.body.password;
+
+    user.save((err) => {
+        if (err) { return next(err); }
+
+        res.send({ user });
+    })
+
+})
 
 const PORT = process.env.PORT || 8080;
-
 const server = http.createServer(app);
 
 server.listen(PORT, (err) => {
