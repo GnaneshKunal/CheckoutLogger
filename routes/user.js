b8e5397d1ce7e55ce21ef2a0187b23e39dbd5f7f
gnaneshkunal <gnaneshpingu@gmail.com>
2017-02-05 16:14:57 +0530

    flashing errors & lowercasing email

--- routes/user.js
+++ routes/user.js
@@ -2,11 +2,13 @@ const router = require('express').Router();
 const User = require('../models/user');
 
 router.get('/signup', (req, res, next) => {
-    res.render('accounts/signup');
+    res.render('accounts/signup', {
+        errors: req.flash('errors')
+    });
 });
 
 router.post('/signup', (req, res, next) => {
-    const email = req.body.email;
+    const email = req.body.email.toLowerCase();
     const name = req.body.name;
     const password = req.body.password;
     console.log(req.body);
@@ -22,16 +24,13 @@ router.post('/signup', (req, res, next) => {
     });
     User.findOne({ email: email }, function(err, existingUser) {
         if (existingUser) {
-            console.log(req.body.email + " already exists");
-            res.redirect('/signup');
+            req.flash('errors', 'Account with that email address already exists');
+            return res.redirect('/signup');
         } else {
             user.save((err, user) => {
                 if (err) return next(err);
 
-                res.json({ 
-                    success: "New user has been created",
-                    user
-                });
+                return res.redirect('/');
             });
         }
     });
