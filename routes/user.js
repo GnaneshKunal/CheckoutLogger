9ce34e36297d337687de1fb398142685077e6943
gnaneshkunal <gnaneshpingu@gmail.com>
2017-02-05 18:49:02 +0530

    logout route

--- routes/user.js
+++ routes/user.js
@@ -58,4 +58,9 @@ router.post('/signup', (req, res, next) => {
     });
 });
 
+router.get('/logout', (req, res, next) => {
+    req.logout();
+    res.redirect('/');
+});
+
 module.exports = router;
