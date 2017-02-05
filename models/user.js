239bbf56a5ed6603664e730d8a73125e6e0596bb
gnaneshkunal <gnaneshpingu@gmail.com>
2017-02-05 21:19:19 +0530

    generating gravatar

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt-nodejs');
+const crypto = require('crypto');
 
 mongoose.Promise = global.Promise;
 
@@ -37,5 +38,12 @@ UserSchema.pre('save', function(next) {
 UserSchema.methods.comparePassword = function(password) {
     return bcrypt.compareSync(password,this.password);
 };
+
+UserSchema.methods.gravatar = function(size) {
+    if (!this.size) size = 200;
+    if (!this.email) return 'https://gravatar.com/avatar/?s' + size + '&d=retro';
+    var md5 = crypto.createHash('md5').update(this.email).digest('hex');
+    return 'https://gravatar.com/avatar/' + md5 + '?s=' + size + '&d=retro';
+}
 
 module.exports = mongoose.model('User', UserSchema);
