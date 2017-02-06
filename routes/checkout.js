4845bd1be8099dfd87e51d250d5c53966dcfce06
gnaneshkunal <gnaneshpingu@gmail.com>
2017-02-06 19:45:53 +0530

    rendering checkout routes

--- routes/checkout.js
+++ routes/checkout.js
@@ -5,3 +5,11 @@ router.get('/checkout', (req, res) => {
 });
 
 module.exports = router;
+
+router.get('/checkout-new', (req, res) => {
+    res.render('checkouts/checkout-new');
+});
+
+router.get('/checkout-history', (req, res) => {
+    res.render('checkouts/checkout-history');
+});
