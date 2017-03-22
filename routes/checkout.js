dbdab4cb6460d8be84fa91a05a259870d2ba56a3
gnaneshkunal <gnaneshpingu@gmail.com>
2017-03-22 18:04:21 +0530

    sort decs and new checkout

--- routes/checkout.js
+++ routes/checkout.js
@@ -1,19 +1,24 @@
 const router = require('express').Router();
 const fs = require('fs');
 const mongoose = require('mongoose');
+const multer = require('multer');
+const path = require('path');
 const Checkout = require('../models/checkout');
+const parser = require('../lib/parser');
+let upload = multer({dest: '/tmp/' });
+let exampleJSON = require('../lib/examples/star.json');
 
 function paginate(req, res, next) {
     let perPage = 5;
     let page = req.params.page - 1;
     Checkout.find({ bill_owner: req.user._id })
+        .sort({ date: 'desc' })
         .skip(perPage * page)
         .limit(perPage)
         .populate('bill_owner', 'profile.name')
         .exec((err, checkouts) => {
             if (err) return next(err);
             Checkout.count().exec((err, count) => {
-                console.log(count);
                 if (err) return next(err);
                 if (!checkouts)
                     return res.render('checkouts/checkout-history', { checkouts: [], pages: count / perPage });
@@ -54,6 +59,44 @@ router.get('/checkout-new', (req, res, next) => {
     return res.render('checkouts/checkout-new');
 });
 
+router.post('/checkout-new', upload.single('checkout'),(req, res, next) => {
+    if (!req.user) return res.redirect('/');
+
+    if (req.file) {
+        let extensions = ['.png', '.jpg'];
+        if (extensions.indexOf(path.extname(req.file.originalname)) !== -1) {
+            console.log(req.file);
+            let file = path.join(path.dirname(__dirname), 'public/uploads/checkouts', req.file.filename);
+            //vision(req.file, config,(err, text) => {
+                let detectedText = exampleJSON.textAnnotations[0].description;
+                let textArray = detectedText.split('\n');
+                let title = textArray[0];
+                let total = parser.parseT(textArray, "TOTAL", "Total", "TOTAL NET");
+                let total_tax = parser.parseT(textArray, "TAX", "Tax", "TVA");
+                let date = parser.parseDate(textArray);
+                let location = "chittoor";
+                let description = "Checkout";
+                let bill_picture = path.join('upload/checkouts', req.file.filename);
+                var checkout = new Checkout({
+                    bill_id: req.file.filename,
+                    title,
+                    description,
+                    date,
+                    location,
+                    total_tax,
+                    total,
+                    bill_picture,
+                    bill_owner: req.user._id
+                });
+                checkout.save((err) => {
+                    if (err) return next(err);
+                    fs.renameSync(req.file.path, file);
+                    return res.redirect('/checkout/' + checkout._id);
+                });
+        }
+    } 
+});
+
 router.get('/checkout-history', (req, res, next) => {
     if (!req.user) return res.redirect('/');
     paginate(req, res, next);
@@ -62,34 +105,4 @@ router.get('/checkout-history', (req, res, next) => {
 router.get('/checkout-history/:page', (req, res, next) => {
     if (!req.user) return res.redirect('/');
     paginate(req, res, next);
-});
-
-router.post('/api/checkout', (req, res, next) => {
-    // bill_id: { type: String, unique: true },
-	// title: { type: String, lowercase: true },
-	// description: { type: String, default: ''},
-	// date: { type: Date, default: Date() },
-	// location: { type: String, default: '' },
-	// total_tax: { type: String },
-	// bill_picture: { type: String, required: true },
-	// bill_owner: { type: Schema.Types.ObjectId, ref: "User" }
-    const bill_id = req.body.bill_id;
-    const title = req.body.title;
-    const description = req.body.description;
-    const total_tax = req.body.total_tax;
-
-    var checkout = new Checkout({
-        bill_id,
-        title,
-        description,
-        date: Date(),
-        location: 'https://www.google.com/maps/place/Starbucks+Coffee/@17.4506728,78.3786222,13z/data=!4m8!1m2!2m1!1sstarbucks+near+Hyderabad,+Telangana,+India!3m4!1s0x0:0xd0691d7fbe930877!8m2!3d17.484271!4d78.3892322?hl=en',
-        total_tax,
-        bill_picture: '/uploads/pictures/2b85a9585550716c2ec9cde481cd4ddd',
-        bill_owner: '5897777c77a3897b97677d4f'
-    });
-    checkout.save((err) => {
-        if (err) { return next(err); }
-        return res.json(checkout);
-    })
 });
